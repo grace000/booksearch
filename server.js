44afc1a18685c6556bb5d0c76f2ac41845546d11
Tiffani <tiffani.grace.elmore@gmail.com>
2018-10-16 21:37:39 -0500

    remove api env variable from server

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const path = require('path');
-require('dotenv').config();
-
-const api = process.env.REACT_APP_API_URL;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
