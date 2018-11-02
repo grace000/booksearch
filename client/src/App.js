867bf8506d92812ec97aa6d1aa073808a609c87d
Tiffani <tiffani.grace.elmore@gmail.com>
2018-11-01 21:36:04 -0500

    update app entry

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
-import Query from './components/Query/Query';
-import Result from './components/Result/Result';
-import { runQuery } from './components/utility/helpers';
+import React, { Component } from 'react'
+import './App.css'
+import Home from "./scenes/Home"
 
-class App extends Component {
-  state = {
-    results:[],
-    errorStatus: undefined
-  }
-
-  getBooks = (e) => {
-    e.preventDefault();
-    const term = e.target.elements.search.value;
-    e.target.reset();
-
-    runQuery(term)
-      .then(data => {
-        this.setState({results:[]})
-        const bookList = data.items;
-        this.setState({results:bookList})
-      })
-      .catch(error => {
-          this.setState({errorStatus: "search returned no results"})
-      })
-  }
+export default class App extends Component {
   render() {
-    const { results, errorStatus } = this.state;
     return (
       <div className="App">
-        <div className="query-container">
-          <Query getBooks={this.getBooks} />
-        </div>
-        <div className="results-container">
-          {errorStatus && <p>{errorStatus}</p>}
-          {results.length === 0 && <p>Start your book search!</p>}
-          {results.map(result => <Result 
-            key={result.id}
-            result={result.volumeInfo}
-          />)}
-        </div>
+        <Home />
       </div>
-    );
+    )
   }
 }
-
-export default App;
